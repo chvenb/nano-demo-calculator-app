ea7e466d49d2c7e56f9bc9854d995d8a82b4f088
chvenb <chvenbalasubramanyam@gmail.com>
2023-09-06 01:07:36 +0530

    changed add,subtract

--- server.py
+++ server.py
@@ -1,19 +1,32 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+
+    return 'Hello world!'
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+
+    data = request.get_json()
+    sum = data['first'] + data['second']
+    dic = {}
+    dic['result'] = sum
+
+    return dic
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    data = request.get_json()
+    sum = data['first'] - data['second']
+    dic = {}
+    dic['result'] = sum
+
+    return dic
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
